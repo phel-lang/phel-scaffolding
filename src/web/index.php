9e46aebd09512ed89068ad27939270f3b8bf7f59
JesusValera <jesus1902@outlook.com>
2021-01-28 17:24:26 +0100

    Lowercase folders

--- src/web/index.php
+++ src/web/index.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 $runtime = require __DIR__ . '/../../vendor/PhelRuntime.php';
 
-$runtime->loadNs('phel-scaffolding\Web\views\main');
+$runtime->loadNs('phel-scaffolding\web\views\main');
