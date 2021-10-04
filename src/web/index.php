9f53489d7f8aad9eba3f2daca4dd61e87bed2465
Chemaclass <chemaclass@outlook.es>
2021-10-04 21:49:36 +0200

    Add Gacela::bootstrap() to index.php

--- src/web/index.php
+++ src/web/index.php
@@ -2,9 +2,18 @@
 
 declare(strict_types=1);
 
+use Gacela\Framework\Gacela;
 use Phel\Run\RunFacade;
 
-require dirname(__DIR__) . '/../vendor/autoload.php';
+$projectRootDir = dirname(__DIR__, 2) . '/';
+
+require $projectRootDir . 'vendor/autoload.php';
+
+Gacela::bootstrap($projectRootDir, [
+    'config' => [
+        'path' => 'phel-config.php',
+    ],
+]);
 
 $runFacade = new RunFacade();
 $runFacade->runNamespace('phel-scaffolding\web\views\main');
